dcfcc433f460dedcba2ec52e0febb83bd1268a05
xiang <xiangquan3721@163.com>
2019-10-16 16:05:15 +0800

    1500W_750W_FORTEST_20160928 IGBT defineROHM PCB4.0

--- 1500W_750W_FORTEST_code/1500W_750W_FORTEST/Common/UserDefinition/Userdef.h
+++ 1500W_750W_FORTEST_code/1500W_750W_FORTEST/Common/UserDefinition/Userdef.h
@@ -112,8 +112,8 @@
 //#define PHASE_INC_CALC 448000UL  //((DWORD)(FCY/((DWORD)PDIV*2*FPWM))*65536) // For 20Khz
 
 /* Half of the PWM Deadtime; The effective deadtime written in ALTDTR registers is 2*ALTDTR_DIV2 */
-   #define	ALTDTR_DIV2_750W	140//120
-   #define	ALTDTR_DIV2_1500W   140//700
+#define	ALTDTR_DIV2_750W	140//120
+#define	ALTDTR_DIV2_1500W   140//700
 //#define	ALTDTR_DIV2	280//(70*4)      //5us
 //#define	ALTDTR_DIV2	147//(70*2.1)      //2.1us
 //#define	ALTDTR_DIV2	210//(70*3)      //3us
