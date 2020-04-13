7c041e24659dd12afe9011d1d7f9524ee7ad4fb0
xiang <xiangquan3721@163.com>
2020-04-13 14:38:53 +0800

    M1 DRIVE 20180718(Ver_1804_7)ROHM

---
+++ M1 DRIVE_code/SHEET SHUTTER DRIVE/DriveBLDC750W.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DriveBLDC750W.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DriveBLDC750W.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=drivebldc750w.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/drivebldc750w.x.tar
