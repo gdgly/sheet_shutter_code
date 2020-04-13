594bb2861af10014197ba7fc4b9bc8ad72095b17
xiang <xiangquan3721@163.com>
2020-04-13 14:30:32 +0800

    M1 DRIVE 20171114(Ver_1711.0) ROME PCB3.0 R0.1

--- M1 DRIVE_code/SHEET SHUTTER DRIVE/Common/UserDefinition/Userdef.h
+++ M1 DRIVE_code/SHEET SHUTTER DRIVE/Common/UserDefinition/Userdef.h
@@ -22,6 +22,7 @@
 #ifndef USERDEF_H
 #define USERDEF_H
 #include "./Common/Typedefs/Typedefs.h"
+#include "./Common/Extern/Extern.h"
 
 #define USE_PHASE_INC_AND_CORRECTION
 #define PICOMO_MOTOR        1//not use
@@ -104,7 +105,12 @@
 //#define PHASE_INC_CALC 746667UL  //((DWORD)(FCY/((DWORD)PDIV*2*FPWM))*65536) 
 
 /* Half of the PWM Deadtime; The effective deadtime written in ALTDTR registers is 2*ALTDTR_DIV2 */
-#define	ALTDTR_DIV2	140//120
+#ifdef IGBT_HighActive_ROME
+   #define	ALTDTR_DIV2	196  //2.8us
+#endif
+#ifdef IGBT_LowActive_IR
+   #define	ALTDTR_DIV2	140//120
+#endif
 //#define	ALTDTR_DIV2	280//(70*4)      //5us
 //#define	ALTDTR_DIV2	147//(70*2.1)      //2.1us
 //#define	ALTDTR_DIV2	210//(70*3)      //3us
